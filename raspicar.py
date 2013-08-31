672bd96b9baa6a16e1c19e4b3564b4d148d8e6a8
Alexander Shchapov <alexanderad@gmail.com>
2013-09-01 01:00:31 +0300

    Simple menu.

--- raspicar.py
+++ raspicar.py
@@ -53,78 +53,136 @@ def status_screen(message=''):
     line2 = '%-8s%8s' % (temp, disk_space)
     return '%s\n%s' % (line1, line2)
 
-def network_status_screen():
-    """ Network status screen """
-    return '%s' % get_ip_address()
-
-class VideoRecorder():
-    _active = False
-    _pid = None
-    _timer = None
-    _quality_map = {
-        '1080.15': {'h': 1080, 'w': 1920, 'b': '15000000'},
-        '720p.5': {'h': 720, 'w': 1280, 'b': '5000000'},
-        '720p.2': {'h': 720, 'w': 1280, 'b': '2000000'},
-        '720p.5': {'h': 720, 'w': 1280, 'b': '5000000'},
-        '480p.4': {'h': 480, 'w': 854, 'b': '4000000'},
-    }
-
-    def _run_and_get_pid(self, command):
-        """ Run command and return PID """
-        process = subprocess.Popen(command, shell=True)
-        return process.pid
-
-    def stop(self):
-        """ Stop video recoding immediately """
-        self._timer.cancel()
-        self._run_and_get_pid("kill -TERM %s" % self._pid)
-        self._active = False
-        print "recoder stopped"
-
-    def start(self, quality, duration):
-        """ Start recording a video of quality and duration """
-        # organize params for raspivid command
-        filename = '%s/%s_%s.h264' % (VIDEO_DESTINATION,
-                                      quality,
-                                      datetime.now().strftime("%Y.%m.%d_%H%M"))
-        params = {"t": duration * 1000,
-                  "filename": filename}
-        params.update(self._quality_map.get(quality))
-        raspivid = "raspivid -hf -vf -vs -w %(w)s -h %(h)s -fps 25 -b %(b)s -t %(t)s -o %(filename)s" % params
-        print "raspivid command!", raspivid
-    
-        self._pid = self._run_and_get_pid("sleep 10")
-        print "pid is", self._pid
-        self._active = True
-    
-        # don't interrupt raspivid, it will finish itself
-        # we're just interested when recording is finished
-        self._timer = Timer(duration + 1, self.stop)
-        self._timer.start()
-        print "recorder started"
-
-    def is_active(self):
-        return self._active
-
 if __name__ == '__main__':
     # initialize LCD
     lcd = Adafruit_CharLCDPlate.Adafruit_CharLCDPlate()
     lcd.begin(16, 2)
 
     # a bit of fancy colors
-    colors = (lcd.ON , lcd.YELLOW, lcd.GREEN, lcd.TEAL, lcd.GREEN)
+    colors = (lcd.ON , lcd.GREEN, lcd.TEAL, lcd.GREEN)
 
     # some variables to be set once
     lastTime = 0
+    state = "menu"
     message = "Ready"
 
     # set random backlight color
     lcd.backlight(choice(colors))
     lcd.clear()
-
-    #recorder = VideoRecorder()
-    #recorder.start('720p', 5)
-
+    
+    # create characters
+    lcd.createChar(0, [0b0, 0b100, 0b1110, 0b11111, 0b0, 0b0, 0b0, 0b0]) # arrow up
+    lcd.createChar(1, [0b0, 0b0, 0b0, 0b11111, 0b1110, 0b100, 0b0, 0b0]) # arrow down
+    lcd.createChar(2, [0b0, 0b10, 0b110, 0b1110, 0b110, 0b10, 0b0, 0b0]) # arrow left
+    lcd.createChar(3, [0b0, 0b1000, 0b1100, 0b1110, 0b1100, 0b1000, 0b0, 0b0]) # arrow right
+    lcd.CHAR_ARROW_UP = '\x00'
+    lcd.CHAR_ARROW_DOWN = '\x01'
+    lcd.CHAR_ARROW_LEFT = '\x02'
+    lcd.CHAR_ARROW_RIGHT = '\x03'
+    
+    # menu
+    menu = {
+        "level": [],
+        "selected": 0,
+        "items": [
+            {
+                "name": "Recording",
+                "selected": 0,
+                "items": [
+                    {"name": "Record 1080p", "description": "15 Mbits"},
+                    {"name": "Record 720p", "description": "7 Mbits"},
+                    {"name": "Record 480p", "description": "4 Mbits"},
+                ]
+            },
+            {
+                "name": "Music",
+            },
+            {
+                "name": "Information",
+                "selected": 0,
+                "items": [
+                    {"name": "Network"},
+                    {"name": "Disk space"},
+                    {"name": "Version"},
+                ]
+            },
+            {
+                "name": "Settings",
+                "selected": 0,
+                "items": [
+                    {
+                        "name": "Backlight",
+                        "selected": 0,
+                        "items": [
+                            {"name": "White", },
+                            {"name": "Green", },
+                            {"name": "Teal", },
+                            {"name": "Yellow", },
+                            {"name": "Red", },
+                            {"name": "Violet", },
+                            {"name": "Blue", },
+                            {"name": "Off", },
+                        ]
+                    },
+                    {
+                        "name": "Video length",
+                        "selected": 0,
+                        "items": [
+                            {"name": "5 mins"},
+                            {"name": "10 mins"},
+                            {"name": "20 mins"},
+                            {"name": "30 mins"},
+                            {"name": "60 mins"},
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+    
+    def redraw_menu(btn):
+        
+        # are we allowed to operate?
+        if "menu" != state:
+            return
+        
+        # first, allow to go up if it's possible and requested
+        if btn == "up":
+            if menu["level"]:
+                menu["level"].pop()
+                
+        # second, go down to the selected item
+        item = menu
+        for level in menu["level"]:
+            item = item["items"][level]
+            
+        # third, allow to go even lower, if it's possible and requested
+        if btn in ["down", "select"]:
+            if "items" in item["items"][item["selected"]]:
+                menu["level"].append(item["selected"])
+                item = item["items"][item["selected"]]
+            
+        # allow to go through items of this level
+        if btn in ["left", "right"]:
+            if "left" == btn:
+                delta = -1
+            if "right" == btn:
+                delta = +1
+            item["selected"] = (item["selected"] + delta) % len(item["items"])
+        
+        # prepare item meta to be printed to screen
+        name = item["items"][item["selected"]]["name"]
+        description = item["items"][item["selected"]].get("description", "")
+        position = '%s%d/%d%s' % (lcd.CHAR_ARROW_LEFT,
+                                  item["selected"] + 1, len(item["items"]),
+                                  lcd.CHAR_ARROW_RIGHT)
+        
+        lcd.home()
+        lcd.clear()
+        line1 = '%-15s%1s' % (name, lcd.CHAR_ARROW_UP if menu["level"] else "")
+        line2 = '%-11s%5s' % (description[:11], position)
+        lcd.message('%s\n%s' % (line1, line2))
+        
     # main cylce
     while True:
         # poll all buttons once, avoids repeated I2C traffic for different cases
@@ -135,20 +193,23 @@ if __name__ == '__main__':
         btn_right = b & (1 << lcd.RIGHT)
         btn_select = b & (1 << lcd.SELECT)
         
+        
         if btn_up:
-            message = "up"
+            redraw_menu("up")
         elif btn_down:
-            message = "down"
+            redraw_menu("down")
         elif btn_left:
-            message = "left"
+            redraw_menu("left")
         elif btn_right:
-            message = "right"
+            redraw_menu("right")
         elif btn_select:
             message = "select"
+            redraw_menu("123")
         
-        # update status message
-        lcd.home()
-        lcd.message(status_screen(message))
+        if state == "idle":
+            # update status message
+            lcd.home()
+            lcd.message(status_screen(message))
     
         # throttle frame rate, keeps screen legible
         while True:
